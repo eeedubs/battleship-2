e13a939fce603ea932af18f8ab8c52023e56d3c3
Eric Wilson <eric.wilson@wealthbar.com>
2020-04-24 16:13:40 -0700

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Banner />
+    <Home />
+  </div>
+</template>
+
+<script>
+import { Home, Banner } from './components';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    Banner
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 0;
+}
+</style>
